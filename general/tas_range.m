444bc06875c177d1658c82e72ecf74d5e83d6a04
Thomas Mosier <mosier.thomas@gmail.com>
2018-02-17 15:13:04 -0500

    changed name of variable in tas_range function

--- general/tas_range.m
+++ general/tas_range.m
@@ -1,12 +1,12 @@
-function tasDelta = tas_range(tasMax, tasMin)
+function tasRange = tas_range(tasMax, tasMin)
 
 szTas = size(tasMax);
-tasDelta = nan(szTas, 'single');
+tasRange = nan(szTas, 'single');
 
 for ii = 1 : szTas(1)
     if ii ~= szTas(1)
-        tasDelta(ii,:,:) = squeeze(tasMax(ii,:,:)) - squeeze(min(tasMin([ii,ii+1], :, :), [], 1));
+        tasRange(ii,:,:) = squeeze(tasMax(ii,:,:)) - squeeze(min(tasMin([ii,ii+1], :, :), [], 1));
     else
-        tasDelta(ii,:,:) = squeeze(tasMax(ii,:,:)) - squeeze(tasMin(ii,:,:));
+        tasRange(ii,:,:) = squeeze(tasMax(ii,:,:)) - squeeze(tasMin(ii,:,:));
     end
 end
